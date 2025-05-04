fe165212c267a66c671e8ba0b622d013c453602b
Rufus Vijayaratnam <rufusvijayaratnam@gmail.com>
2025-05-04 14:37:36 +0100

    Added  to title field of roxygen2 template for cursor default location.

--- lua/neogen/templates/roxygen2.lua
+++ lua/neogen/templates/roxygen2.lua
@@ -1,7 +1,7 @@
 local i = require("neogen.types.template").item
 
 return {
-    { nil, "#' @title", { no_results = false, type = { "func" } } },
+    { nil, "#' @title $1", { no_results = false, type = { "func" } } },
     { nil, "#' @description", { no_results = false, type = { "func" } } },
     { i.Parameter, "#' @param %s" },
     { nil, "#' @return", { type = { "func" }, no_results = false } },
